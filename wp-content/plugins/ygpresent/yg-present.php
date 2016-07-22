741e9031fe09a41e4df233ce72536a0c0f58b4ed
Seungmin Lee <w130pmpo@gmail.com>
2016-07-22 18:16:19 -0400

    - wp-migrate-db added - yg present plugin 'characters of unexpected output ' bug fix

--- wp-content/plugins/ygpresent/yg-present.php
+++ wp-content/plugins/ygpresent/yg-present.php
@@ -7,215 +7,145 @@ Author: Zeter Lee
 Description: YG Present
  */
 
-//if ( !function_exists( 'add_artist_custom_post_type' ) ) {
-//    function add_artist_custom_post_type() {
-//        register_post_type( 'artist', array(  'label' => 'Artist', 'description' => 'YG Artist', 'public' => true, 'show_ui' => true, 'show_in_menu' => true, 'capability_type' => 'post', 'hierarchical' => false, 'rewrite' => array( 'slug' => '' ), 'query_var' => true, 'exclude_from_search' => false, 'supports' => array( 'title', 'editor', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'thumbnail', 'author', 'page-attributes', ), 'labels' => array (
-//            'name' => 'Artist',
-//            'singular_name' => 'Artist',
-//            'menu_name' => 'Artist',
-//            'add_new' => 'Add Artist',
-//            'add_new_item' => 'Add New Artist',
-//            'edit' => 'Edit',
-//            'edit_item' => 'Edit Artist',
-//            'new_item' => 'New Artist',
-//            'view' => 'View Artist',
-//            'view_item' => 'View Artist',
-//            'search_items' => 'Search Artist',
-//            'not_found' => 'No Artist Found',
-//            'not_found_in_trash' => 'No Artist Found in Trash',
-//            'parent' => 'Parent Artist',
-//        ), ) );
-//
-//    }
-//    add_action( 'init', 'add_artist_custom_post_type' );
-//}
-//
-//
-//if ( !function_exists( 'add_ticket_custom_post_type' ) ) {
-//    function add_ticket_custom_post_type() {
-//        register_post_type( 'ticket', array(  'label' => 'Ticket', 'description' => 'YG Ticket', 'public' => true, 'show_ui' => true, 'show_in_menu' => true, 'capability_type' => 'post', 'hierarchical' => false, 'rewrite' => array( 'slug' => '' ), 'query_var' => true, 'exclude_from_search' => false, 'supports' => array( 'title', 'editor', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'thumbnail', 'author', 'page-attributes', ), 'labels' => array (
-//            'name' => 'Ticket',
-//            'singular_name' => 'Ticket',
-//            'menu_name' => 'Ticket',
-//            'add_new' => 'Add Ticket',
-//            'add_new_item' => 'Add New Ticket',
-//            'edit' => 'Edit',
-//            'edit_item' => 'Edit Ticket',
-//            'new_item' => 'New Ticket',
-//            'view' => 'View Ticket',
-//            'view_item' => 'View Ticket',
-//            'search_items' => 'Search Ticket',
-//            'not_found' => 'No Ticket Found',
-//            'not_found_in_trash' => 'No Ticket Found in Trash',
-//            'parent' => 'Parent Ticket',
-//        ), ) );
-//
-//    }
-//    add_action( 'init', 'add_ticket_custom_post_type' );
-//}
-//
-//
-//if ( !function_exists( 'add_event_custom_post_type' ) ) {
-//    function add_event_custom_post_type() {
-//        register_post_type( 'event', array(  'label' => 'Event', 'description' => 'YG Event', 'public' => true, 'show_ui' => true, 'show_in_menu' => true, 'capability_type' => 'post', 'hierarchical' => false, 'rewrite' => array( 'slug' => '' ), 'query_var' => true, 'exclude_from_search' => false, 'supports' => array( 'title', 'editor', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'thumbnail', 'author', 'page-attributes', ), 'labels' => array (
-//            'name' => 'Event',
-//            'singular_name' => 'Event',
-//            'menu_name' => 'Event',
-//            'add_new' => 'Add Event',
-//            'add_new_item' => 'Add New Event',
-//            'edit' => 'Edit',
-//            'edit_item' => 'Edit Event',
-//            'new_item' => 'New Event',
-//            'view' => 'View Event',
-//            'view_item' => 'View Event',
-//            'search_items' => 'Search Event',
-//            'not_found' => 'No Ticket Found',
-//            'not_found_in_trash' => 'No Ticket Found in Trash',
-//            'parent' => 'Parent Event',
-//        ), ) );
-//    }
-//    add_action( 'init', 'add_event_custom_post_type' );
-//}
-//
-//
-//
-///**
-// *
-// * How to create custom post meta boxes in WORDPRESS
-// *
-// */
-//
-//add_action( 'load-post.php', 'tour_post_meta_boxes_setup' );
-//add_action( 'load-post-new.php', 'tour_post_meta_boxes_setup' );
-//
-//
-///* Meta box setup function. */
-//function tour_post_meta_boxes_setup() {
-//
-//    /* Add meta boxes on the 'add_meta_boxes' hook. */
-//    add_action( 'add_meta_boxes', 'tour_add_post_meta_boxes' );
-//
-//    /* Save post meta on the 'save_post' hook. */
-//    add_action( 'save_post', 'tour_save_post_class_meta', 10, 2 );
-//}
-//
-///* Create one or more meta boxes to be displayed on the post editor screen. */
-//function tour_add_post_meta_boxes(){
-//    add_meta_box(
-//        'tour-guest', // Unique ID
-//        esc_html('Guest' , 'example'),  // Title
-//        'tour_post_meta_box',  //callback function
-//        'ticket',  // Admin Page or post Type
-//        'advanced', // Context
-//        'default' // Priority
-//    );
-//}
-//
-///* Display the post meta box. */
-//function tour_post_meta_box($object, $box){ ?>
-<!---->
-<!--    --><?php //wp_nonce_field( basename( __FILE__ ), 'tour_post_class_nonce' ); ?>
-<!---->
-<!--    <p>-->
-<!--        <label for="tour-guest">--><?php //_e( "Add Guest Artist", 'example' ); ?><!--</label>-->
-<!--        <br />-->
-<!--        <input class="widefat" type="text" name="tour-guest" id="tour-guest" value="--><?php //echo esc_attr( get_post_meta( $object->ID, 'tour-guest', true ) ); ?><!--" size="30" />-->
-<!---->
-<!--        <input class="widefat" type="text" name="tour-guest-id" id="tour-guest-id" value="--><?php //echo esc_attr( get_post_meta( $object->ID, 'tour-guest-id', true ) ); ?><!--" size="30" />-->
-<!--    </p>-->
-<!--    --><?php
-//}
-//
-///* Save the meta box's post metadata. */
-//function tour_save_post_class_meta($post_id , $post){
-//
-//    /* Verify the nonce before proceeding. */
-//    if ( !isset( $_POST['tour_post_class_nonce'] ) || !wp_verify_nonce( $_POST['tour_post_class_nonce'], basename( __FILE__ ) ) )
-//        return $post_id;
-//
-//    /* Get the post type object. */
-//    $post_type = get_post_type_object($post->post_type);
-//
-//    /* Check if the current user has permission to edit the post. */
-//    if( !current_user_can($post_type->cap->edit_post, $post_id) )
-//        return $post_id;
-//
-//    /* Get the posted data and sanitize it for use as an HTML class. */
-//
-//    $new_meta_value = (isset($_POST['tour-guest']) ? sanitize_html_class( $_POST['tour-guest'] ) : '' );
-//
-//    /* Get the meta key. */
-//    $meta_key = 'tour-guest';
-//
-//
-//    /* Get the meta value of the custom field key. */
-//    $meta_value = get_post_meta($post_id, $meta_key, true);
-//
-//    error_log($new_meta_value);
-//    error_log($meta_key);
-//
-//    /* If a new meta value was added and there was no previous value, add it. */
-//    if ( $new_meta_value && '' == $meta_value )
-//        add_post_meta( $post_id, $meta_key, $new_meta_value, true );
-//
-//    /* If the new meta value does not match the old value, update it. */
-//    elseif ( $new_meta_value && $new_meta_value != $meta_value )
-//        update_post_meta( $post_id, $meta_key, $new_meta_value );
-//
-//    /* If there is no new meta value but an old value exists, delete it. */
-//    elseif ( '' == $new_meta_value && $meta_value )
-//        delete_post_meta( $post_id, $meta_key, $meta_value );
-//
-//
-//
-//    $new_meta_value = (isset($_POST['tour-guest-id']) ? sanitize_html_class( $_POST['tour-guest-id'] ) : '' );
-//
-//    /* Get the meta key. */
-//    $meta_key = 'tour-guest-id';
-//
-//
-//    /* Get the meta value of the custom field key. */
-//    $meta_value = get_post_meta($post_id, $meta_key, true);
-//
-//    error_log($new_meta_value);
-//    error_log($meta_key);
-//
-//    /* If a new meta value was added and there was no previous value, add it. */
-//    if ( $new_meta_value && '' == $meta_value )
-//        add_post_meta( $post_id, $meta_key, $new_meta_value, true );
-//
-//    /* If the new meta value does not match the old value, update it. */
-//    elseif ( $new_meta_value && $new_meta_value != $meta_value )
-//        update_post_meta( $post_id, $meta_key, $new_meta_value );
-//
-//    /* If there is no new meta value but an old value exists, delete it. */
-//    elseif ( '' == $new_meta_value && $meta_value )
-//        delete_post_meta( $post_id, $meta_key, $meta_value );
-//
-//}
-//
-//
-//
-//
-//
-/////* Filter the post class hook with our custom post class function. */
-////add_filter( 'post_class', 'smashing_post_class' );
-////
-////function smashing_post_class( $classes ) {
-////
-////    /* Get the current post ID. */
-////    $post_id = get_the_ID();
-////
-////    /* If we have a post ID, proceed. */
-////    if ( !empty( $post_id ) ) {
-////
-////        /* Get the custom post class. */
-////        $post_class = get_post_meta( $post_id, 'smashing_post_class', true );
-////
-////        /* If a post class was input, sanitize it and add it to the post class array. */
-////        if ( !empty( $post_class ) )
-////            $classes[] = sanitize_html_class( $post_class );
-////    }
-////
-////    return $classes;
-////}
+$main_contents = 'main_contents';
+$hot_track = 'hot_track';
+$hot_blog = 'hot_blog';
+
+
+$yg_option_fields = array(
+    $main_contents,
+    $hot_track,
+    $hot_blog
+);
+
+// Hook for adding admin menus
+add_action('admin_init', 'yg_admin_init');
+add_action('admin_menu', 'yg_admin_menu');
+
+
+
+function yg_admin_init() {
+    global $yg_option_fields;
+
+    foreach($yg_option_fields as $key => $field){
+        register_setting($field, $field);
+    }
+}
+
+// action function for above hook
+function yg_admin_menu() {
+
+    // Add a new top-level menu (Promotional Contents Manager):
+    add_menu_page('Promotion', 'Promotion', 'administrator', 'contents-manager', 'contents_manager' );
+
+    // Add a submenu to the Promotional Contents Manager Menu
+    add_submenu_page('contents-manager', 'Main Contents', 'Main Contents', 'administrator', 'main-contents', 'get_main_contents');
+    add_submenu_page('contents-manager', 'Hot Track', 'Hot Track', 'administrator', 'hot-track', 'get_hot_track');
+    add_submenu_page('contents-manager', 'Hot Blog', 'Hot Blog', 'administrator', 'hot-blog', 'get_hot_blog');
+
+}
+
+function contents_manager() {
+    echo "<h2>" . 'Promotional Contents Manager' . "</h2>";
+}
+
+
+function get_main_contents() {
+
+    echo "<h1>" . 'Main Contents List' . "</h1>";
+    echo "<h4>" . 'Please select promotional items that need to appear on Main Promotional Page' . "</h4>";
+
+    global $main_contents;
+
+    $combined_posts = get_posts([
+        'post_type' => array(
+           'product','tour','event','album'
+        ),
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+    ]);
+
+    $main_contents_option = get_option($main_contents);
+    option_form($main_contents_option, $combined_posts, $main_contents);
+}
+
+
+function get_hot_track() {
+
+    echo "<h1>" . 'Hot Track List' . "</h1>";
+    echo "<h4>" . 'Please select music items that need to appear on Hot Track List' . "</h4>";
+
+    global $hot_track;
+
+    $music_posts = get_posts([
+        'post_type' => 'product',
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+        'meta_query' => array(
+            array(
+                'key' => '_downloadable',
+                'value' => 'yes'
+            )
+        )
+    ]);
+    $hot_track_option = get_option($hot_track);
+    option_form($hot_track_option, $music_posts, $hot_track);
+}
+
+function get_hot_blog(){
+
+    echo "<h1>" . 'Hot Blog List' . "</h1>";
+    echo "<h4>" . 'Please select Blog that need to appear on Hot Blog List' . "</h4>";
+
+    global $hot_blog;
+
+    $blog_posts = get_posts([
+        'post_type' => 'blog',
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+    ]);
+    $hot_track_option = get_option($hot_blog);
+    option_form($hot_track_option, $blog_posts, $hot_blog);
+}
+
+function option_form($option, $posts, $tag){?>
+
+    <form method="post" action="options.php">
+        <?php settings_fields( $tag ); ?>
+        <table class="form-table">
+            <tr valign="top">
+                <td scope="row" colspan="2">
+                    <?php foreach($posts as $post){
+
+                        $title = $post->post_title;
+                        $id = $post->ID;
+                        $post_type = strtoupper($post->post_type);
+                        ?>
+
+                        <input name="<?php echo $tag ?>[<?php echo $id?>]" type="checkbox"
+                            <?=isset($option[$id]) == '1' ? 'checked' : '' ?>
+                        />
+                        <lable for=<?php echo $tag?>><?php echo $title . ' [' . $post_type . ']' ?></lable><br>
+                    <?php } ?>
+                </td>
+            </tr>
+        </table>
+        <?php submit_button(); ?>
+    </form>
+    <?php
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
