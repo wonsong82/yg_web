596721c42ee6f7c79f732b8e8ea12cfefdd0ed43
won song <wcsong@gobaesong.com>
2016-08-16 15:32:22 -0400

    spinner and favicon

--- wp-content/themes/ygpresents/page-templates/inc/header.php
+++ wp-content/themes/ygpresents/page-templates/inc/header.php
@@ -23,6 +23,23 @@ $staticPageCss = file_exists(ABSPATH . "static/static-{$postName}.css") ?
   <?php echo $staticCss ?>
   <?php echo $staticPageCss ?>
 
+  <!--
+  Favicons:
+     for iOS - Add to homescreen for iPhone and iPad
+     for Android Chrome - Add to Homescreen for android
+     for Windows 8 & 10 - Can pin site on desktop
+     for Sarafi - Pinned Tab
+  -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="/manifest.json">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="apple-mobile-web-app-title" content="YG Presents">
+  <meta name="application-name" content="YG Presents">
+  <meta name="theme-color" content="#ffffff">
+  <!-- End Favicons -->
+
 </head>
 <body>
 
