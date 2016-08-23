3ba220d4a04e85701ce8c7cf9664a4ef66787549
Sean Choi <seanchoi012@gmail.com>
2016-08-23 14:41:25 -0400

    Add menu bar to static template

--- wp-content/themes/ygpresents/page-templates/template-static.php
+++ wp-content/themes/ygpresents/page-templates/template-static.php
@@ -11,8 +11,26 @@ $postName = $post->post_name; // Get uname from current post
 require __DIR__ . '/inc/header.php';
 ?>
 
+
 <article class="static-content <?php echo $post->post_name;?>">
-  <?php the_content() ?>
+  <?php the_content(); ?>
+  <?php
+  $pages = array("my-account");
+  $home_url = get_home_url();
+  if(in_array($post->post_name, $pages) ){
+    echo '<div class="menu-bar">
+            <div class="menu-title">My Account</div>
+            <div class="menu-body">
+                <ul>
+                    <li><a href="'.$home_url.'/my-account/edit-account">Profile</a></li>
+                    <li><a href="'.$home_url.'/order">My Order</a></li>
+                    <li><a href="'.$home_url.'/my-account">My Address</a></li>
+                    <li><a href="'.$home_url.'/wp-login.php?loggedout=true">log out</a></li>  
+                </ul>
+            </div>
+          </div>';
+  }
+  ?>
 </article>
 
 
