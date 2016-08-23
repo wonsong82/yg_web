58cfb78c77d461ca7e5664da9009769b6c3e2935
Sean Choi <seanchoi012@gmail.com>
2016-08-23 18:46:45 -0400

    1. Add title to My order page 2. add my order page to show menu list

--- wp-content/plugins/woocommerce/templates/order/form-tracking.php
+++ wp-content/plugins/woocommerce/templates/order/form-tracking.php
@@ -24,7 +24,8 @@ global $post;
 ?>
 
 <form action="<?php echo esc_url( get_permalink( $post->ID ) ); ?>" method="post" class="track_order">
-
+	<div class="static-content-title">My order</div>
+	<div class="static-content-h2">Order tracking</div>
 	<p><?php _e( 'To track your order please enter your Order ID in the box below and press the "Track" button. This was given to you on your receipt and in the confirmation email you should have received.', 'woocommerce' ); ?></p>
 
 	<p class="form-row form-row-first"><label for="orderid"><?php _e( 'Order ID', 'woocommerce' ); ?></label> <input class="input-text" type="text" name="orderid" id="orderid" placeholder="<?php esc_attr_e( 'Found in your order confirmation email.', 'woocommerce' ); ?>" /></p>
--- wp-content/themes/ygpresents/page-templates/template-static.php
+++ wp-content/themes/ygpresents/page-templates/template-static.php
@@ -15,7 +15,7 @@ require __DIR__ . '/inc/header.php';
 <article class="static-content <?php echo $post->post_name;?>">
   <?php the_content(); ?>
   <?php
-  $pages = array("my-account");
+  $pages = array("my-account","my-orders");
   $home_url = get_home_url();
   if(in_array($post->post_name, $pages) ){
     echo '<div class="menu-bar">
@@ -23,7 +23,7 @@ require __DIR__ . '/inc/header.php';
             <div class="menu-body">
                 <ul>
                     <li><a href="'.$home_url.'/my-account/edit-account">Profile</a></li>
-                    <li><a href="'.$home_url.'/order">My Order</a></li>
+                    <li><a href="'.$home_url.'/my-orders">My Order</a></li>
                     <li><a href="'.$home_url.'/my-account">My Address</a></li>
                     <li><a href="'.$home_url.'/wp-login.php?loggedout=true">log out</a></li>  
                 </ul>
